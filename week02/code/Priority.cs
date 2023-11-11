e70d476a8315d7c239410bde0bc29048f4812ab4
joeverlyn <joeverlyncapute@gmail.com>
2023-11-11 12:59:44 -0700

    Not sure...

--- week02/code/Priority.cs
+++ week02/code/Priority.cs
@@ -12,26 +12,33 @@ public static class Priority {
     public static void Test() {
         // TODO Problem 2 - Write and run test cases and fix the code to match requirements
         // Example of creating and using the priority queue
-        var priorityQueue = new PriorityQueue();
-        Console.WriteLine(priorityQueue);
+         var priorityQueue = new PriorityQueue();
+        Console.WriteLine("Initial Queue: " + priorityQueue);
 
         // Test Cases
 
         // Test 1
-        // Scenario: 
-        // Expected Result: 
-        Console.WriteLine("Test 1");
+        // Scenario: Enqueue values with different priorities and dequeue them
+        // Expected Result: Values should be dequeued in descending order of priority
+        Console.WriteLine("\nTest 1");
 
-        // Defect(s) Found: 
+        priorityQueue.Enqueue("Alice", 3);
+        priorityQueue.Enqueue("Bob", 1);
+        priorityQueue.Enqueue("Charlie", 2);
+
+        Console.WriteLine("After Enqueue: " + priorityQueue);
+        Console.WriteLine("Dequeued: " + priorityQueue.Dequeue()); // Expected: Item1 (Pri:3)
+        Console.WriteLine("Dequeued: " + priorityQueue.Dequeue()); // Expected: Item3 (Pri:2)
+        Console.WriteLine("Dequeued: " + priorityQueue.Dequeue()); // Expected: Item2 (Pri:1)
 
         Console.WriteLine("---------");
 
         // Test 2
-        // Scenario: 
-        // Expected Result: 
-        Console.WriteLine("Test 2");
+        // Scenario: Try to dequeue from an empty queue
+        // Expected Result: Message should be displayed saying the queue is empty
+        Console.WriteLine("\nTest 2");
 
-        // Defect(s) Found: 
+        Console.WriteLine("Dequeued: " + priorityQueue.Dequeue()); // Expected: The queue is empty.
 
         Console.WriteLine("---------");
 
--- week02/code/TakingTurns.cs
+++ week02/code/TakingTurns.cs
@@ -17,65 +17,64 @@ public static class TakingTurns {
         // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3) and
         //           run until the queue is empty
         // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-        Console.WriteLine("Test 1");
-        var players = new TakingTurnsQueue();
-        players.AddPerson("Bob", 2);
-        players.AddPerson("Tim", 5);
-        players.AddPerson("Sue", 3);
-        // Console.WriteLine(players);    // This can be un-commented out for debug help
-        while (players.Length > 0)
-            players.GetNextPerson();
-        // Defect(s) Found: 
-
+         Console.WriteLine("Test 1");
+        var players1 = new TakingTurnsQueue();
+        players1.AddPerson("Bob", 2);
+        players1.AddPerson("Tim", 5);
+        players1.AddPerson("Sue", 3);
+            //Console.WriteLine(players);    // This can be un-commented out for debug help
+        while (players1.Length > 0)
+            players1.GetNextPerson();
         Console.WriteLine("---------");
+
+        
 
         // Test 2
         // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3)
         //           After running 5 times, add George with 3 turns.  Run until the queue is empty.
         // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, George, Sue, Tim, George, Tim, George
         Console.WriteLine("Test 2");
-        players = new TakingTurnsQueue();
-        players.AddPerson("Bob", 2);
-        players.AddPerson("Tim", 5);
-        players.AddPerson("Sue", 3);
+        var players2 = new TakingTurnsQueue();
+        players2.AddPerson("Bob", 2);
+        players2.AddPerson("Tim", 5);
+        players2.AddPerson("Sue", 3);
         for (int i = 0; i < 5; i++) {
-            players.GetNextPerson();
-            // Console.WriteLine(players);
+            players2.GetNextPerson();
+            //Console.WriteLine(players); // This can be un-commented out for debug help
         }
 
-        players.AddPerson("George", 3);
-        // Console.WriteLine(players);
-        while (players.Length > 0)
-            players.GetNextPerson();
-
-        // Defect(s) Found: 
-
+        players2.AddPerson("George", 3);
+            //Console.WriteLine(players);   //This can be un-commented out for debug help
+        while (players2.Length > 0)
+            players2.GetNextPerson();
         Console.WriteLine("---------");
 
         // Test 3
         // Scenario: Create a queue with the following people and turns: Bob (2), Tim (Forever), Sue (3)
         //           Run 10 times.
         // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
+       
         Console.WriteLine("Test 3");
-        players = new TakingTurnsQueue();
-        players.AddPerson("Bob", 2);
-        players.AddPerson("Tim", 0);
-        players.AddPerson("Sue", 3);
-        // Console.WriteLine(players);
+        var players3 = new TakingTurnsQueue();
+        players3.AddPerson("Bob", 2);
+        players3.AddPerson("Tim", 0); // Forever
+        players3.AddPerson("Sue", 3);
+            // Console.WriteLine(players);  //This can be un-commented out for debug help
         for (int i = 0; i < 10; i++) {
-            players.GetNextPerson();
-            // Console.WriteLine(players);
+            players3.GetNextPerson();
+             //Console.WriteLine(players);  //This can be un-commented out for debug help
         }
-        // Defect(s) Found: 
-
         Console.WriteLine("---------");
+
+
 
         // Test 4
         // Scenario: Try to get the next person from an empty queue
         // Expected Result: Error message should be displayed
+  
         Console.WriteLine("Test 4");
-        players = new TakingTurnsQueue();
-        players.GetNextPerson();
-        // Defect(s) Found:
+        var players4 = new TakingTurnsQueue();
+        players4.GetNextPerson();
+        Console.WriteLine("---------");
     }
 }
